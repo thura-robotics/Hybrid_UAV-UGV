cfef2fb59f94b62efeffdaccc65785c4e3fed31b
Hninei-san <hnineisan547@gmail.com>
2025-12-24 02:09:53 +0700

    Add ServoCommand message

---
+++ software/morphobot_ws/src/hybrid_robot_hardware/hybrid_robot_hardware/motor_driver.py
@@ -0,0 +1,5 @@
+#for motor id assing -- ros2 run waveshare_servos set_id --ros-args -p start_id:=1 -p new_id:=<desired_id>
+#check motor id -- ros2 run waveshare_servos read_id --ros-args
+
+# 1-4 : Driving Motors
+#Install ROS 2 control packages
